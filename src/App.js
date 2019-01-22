47c81eff81cd5c6cd8cfee615ddbde72ee323cf9
Arjun Sasikumar <arjuns@yahoo-inc.com>
2019-01-21 20:48:51 -0800

    ReadMe update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import ChatProvider from "./components/chat-provider";
 import Login from "./components/login";
 import "./app.scss";
